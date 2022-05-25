ef93d9968a23c8d963b6dc5e4cea5688e8f5c878
Claudia <c.agosti006@studenti.unibs.it>
2022-05-25 15:34:07 +0200

    aggiornamento funziona!

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
 
     //creazione variabili xij del modello
-    private static GRBVar[][] aggiungiVariabili(GRBModel model, int nVertici) throws GRBException {
+    private static GRBVar[][] aggiungiVariabilix(GRBModel model, int nVertici) throws GRBException {
         GRBVar[][] xij = new GRBVar[nVertici][nVertici];
 
         for (int i = 0; i < vertiveV; i++) {
@@ -18,6 +18,18 @@ public class Main {
         }
         return xij;
     }
+
+    //creazione variabili uij del modello
+    private static GRBVar[] aggiungiVariabiliu(GRBModel model, int nVertici) throws GRBException {
+        GRBVar[] u = new GRBVar[nVertici];
+
+        for (int j = 0; j <= nVertici-2; j++) {
+            u[j] = model.addVar(1, (nVertici-1), 0, GRB.INTEGER, "u"+j);
+        }
+
+        return u;
+    }
+
 
     //aggiunta funzione obiettivo al modello
     private static void aggiungiFunzioneObiettivo(GRBModel model, int [][] cij, GRBVar [][] xij, int nVertici) throws GRBException {
@@ -37,78 +49,149 @@ public class Main {
 
     private static void aggiungiVincolo1(GRBModel model, GRBVar[][] xij,int nVertici) throws GRBException {
 
-        for (int i = 0; i < nVertici; i++){
-            for (int j = 0; j < nVertici; j++){
+        for(int j=0; j<nVertici; j++){
+            GRBLinExpr expr = new GRBLinExpr();
+            for(int i=0; i<nVertici; i++){
                 if(i!=j) {
-                    GRBLinExpr expr = new GRBLinExpr();
                     expr.addTerm(1, xij[i][j]);
-                    model.addConstr(expr, GRB.EQUAL, 1, "vincolo " + i + j);
                 }
             }
+            model.addConstr(expr, GRB.EQUAL, 1, "vincolo1 " + j);
         }
+
     }
 
     private static void aggiungiVincolo2(GRBModel model, GRBVar[][] xij,int nVertici) throws GRBException {
 
-        for (int j = 0; j < nVertici; j++){
-            for (int i = 0; i < nVertici; i++){
+        for (int i = 0; i < nVertici; i++){
+            GRBLinExpr expr = new GRBLinExpr();
+            for (int j = 0; j < nVertici; j++){
                 if(i!=j) {
-                    GRBLinExpr expr = new GRBLinExpr();
                     expr.addTerm(1, xij[i][j]);
-                    model.addConstr(expr, GRB.EQUAL, 1, "vincolo " + i + j);
                 }
             }
+            model.addConstr(expr, GRB.EQUAL, 1, "vincolo2 " + i );
+        }
+    }
+
+    private static void aggiungiVincolo3(GRBModel model, GRBVar[][] xij,int nVertici, GRBVar [] u) throws GRBException {
+        int cost = (nVertici - 1);
+        GRBLinExpr expr5 = new GRBLinExpr();
+        expr5.addConstant(cost);
+        for (int i = 1; i < nVertici; i++){
+            int indicei = i-1;
+            for (int j = 1; j < nVertici; j++){
+                int indicej = j-1;
+                if(i!=j) {
+                    GRBLinExpr expr = new GRBLinExpr();
+                    expr.addTerm(nVertici, xij[i][j]);
+                    expr.addTerm(1, u[indicei]);
+                    expr.addTerm(-1, u[indicej]);
+                    //System.out.println(i+","+j+" "+expr.getCoeff(i));
+                    model.addConstr(expr, GRB.LESS_EQUAL,expr5, "vincolo di con ui e uj" + j);
+                }
+            }
+        }
+    }
+    private static void aggiungiVincolo4(GRBModel model, int nVertici, GRBVar [] u) throws GRBException {
+        int cost = (nVertici - 2);
+        GRBLinExpr expr5 = new GRBLinExpr();
+        expr5.addConstant(cost);
+        for (int i = 1; i <= nVertici-1; i++){
+
+            GRBLinExpr expr = new GRBLinExpr();
+            expr.addTerm(1, u[i-1]);
+            model.addConstr(expr, GRB.LESS_EQUAL, expr5, "vincolo di minore di n-1 " + i);
+            model.addConstr(expr,GRB.GREATER_EQUAL,0, "vincolo maggiore di 1" + i);
         }
     }
 
 
     public static void main (String[]args){
+        int n_vertici = 40;
+        int[][] cij = new int[n_vertici][n_vertici];;//il percorso è simmetrico -> cij=cji
 
-            int[][] xij; //variabile binaria  ed è uguale a 1 se l'arco (i,j) appartiene al circuito, altrimenti xij=0
-            int[][] cij;//il percorso è simmetrico -> cij=cji
-            int arcoA;
+        int verticeV = 2;
+        int a=7; //vincolo1: il costo dei lati incidenti al vertice v sia al massimo il a% del costo totale del ciclo
+        int [] latob ={6, 25};
+        int[] latoc = {36, 1 };
+        int[] latod = {13, 10};
+        int[] latoe= {13, 10};
+        int[] latof = {32, 2};
+        int[] latog = {2, 8};
+        int[] latoh = {16, 39};
+        int[] latoi = {7, 9};
+        int latol = 2;
 
-            int verticeV = 2;
-            int a=7; //vincolo1: il costo dei lati incidenti al vertice v sia al massimo il a% del costo totale del ciclo
-            int [] latob ={6, 25};
-            int[] latoc = {36, 1 };
-            int[] latod = {13, 10};
-            int[] latoe= {13, 10};
-            int[] latof = {32, 2};
-            int[] latog = {2, 8};
-            int[] latoh = {16, 39};
-            int[] latoi = {7, 9};
-            int latol = 2;
+        String nome_file = "coppia25.txt";
 
+        try {
+            // apre in lettura
+            BufferedReader filebuf = new BufferedReader(new FileReader(nome_file));
+            boolean iniziaLettura = false;
+            String next;
+            next = filebuf.readLine();
 
-            String nome_file = "coppia25.txt";
-
-             try {
-                // apre in lettura
-                BufferedReader filebuf = new BufferedReader(new FileReader(nome_file));
-                boolean iniziaLettura = false;
-                String next;
-                next = filebuf.readLine();
-
-                while (next != null) {
-                    String[] riga;
-                    if (iniziaLettura) { // se non e' finito il file
-                        riga = next.split(" ");
-                        cij[Integer.parseInt(riga[0])][Integer.parseInt(riga[1])] = Integer.parseInt(riga[2]);
-                        cij[Integer.parseInt(riga[1])][Integer.parseInt(riga[0])] = Integer.parseInt(riga[2]);
-                    } else if (next.contains("Vertici")) {
-                        //riga = (next.split(" "));
-                        //cij=new int[riga[1]][riga[1]];
-                        iniziaLettura = true;
-                    }
-                    next = filebuf.readLine();
+            while (next != null) {
+                String[] riga;
+                if (iniziaLettura) { // se non e' finito il file
+                    riga = next.split(" ");
+                    cij[Integer.parseInt(riga[0])][Integer.parseInt(riga[1])] = Integer.parseInt(riga[2]);
+                    cij[Integer.parseInt(riga[1])][Integer.parseInt(riga[0])] = Integer.parseInt(riga[2]);
+                } else if (next.contains("Vertici")) {
+                    //riga = (next.split(" "));
+                    //n_vertici= riga[1];
+                    //cij=new int[riga[1]][riga[1]];
+                    iniziaLettura = true;
                 }
-
-                filebuf.close(); // chiude il file
-
-            } catch (IOException e) {
-                System.out.println(e);
+                next = filebuf.readLine();
             }
+            filebuf.close(); // chiude il file
+        } catch (IOException e) {
+            System.out.println(e);
         }
+
+        GRBEnv env;
+        try {
+            env = new GRBEnv("elaborato1Gurobi.log");
+            //env.set(GRB.IntParam.Presolve, 0);
+            //env.set(GRB.IntParam.Method, 0);
+
+            GRBModel model = new GRBModel(env);
+
+//-------------- AGGIUNTA VARIABILI -----------------------------------
+            GRBVar[][] xij = aggiungiVariabilix(model, n_vertici); //variabile binaria  ed è uguale a 1 se l'arco (i,j) appartiene al circuito, altrimenti xij=0
+            GRBVar[] u = aggiungiVariabiliu(model, n_vertici);
+// --------------------------------------------------------------------
+// -------------- FUNZIONE OBIETTIVO -------------------------------------
+            aggiungiFunzioneObiettivo(model, cij, xij, n_vertici);    //modulo linearizzato
+//------------------------------------------------------------------------
+//-------------------- VINCOLI -----------------------------------------------------
+            aggiungiVincolo1(model, xij, n_vertici);
+            aggiungiVincolo2(model, xij, n_vertici);
+            aggiungiVincolo3(model, xij, n_vertici, u);
+            //aggiungiVincolo4(model, n_vertici, u);
+
+            model.optimize();    //ottimizzazio
+//-----------------------------------------------------------------------------------
+
+            //******************************************************************************************************************
+            System.out.println("\nGRUPPO gruppo_25");
+            System.out.println("Componenti: Brognoli e Agosti\n\n");
+//------------------------------- QUESITO 1 ---------------------------------------------------------------------------
+            System.out.println("QUESITO I:");
+            System.out.println("funzione obiettivo = " + model.get(GRB.DoubleAttr.ObjVal));
+
+
+
+
+
+        } catch (GRBException e){
+            e.printStackTrace();
+        }
+
+
+
     }
+}*/
 
