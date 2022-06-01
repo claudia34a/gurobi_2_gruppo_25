fb401a24df04bddc442362d63de693bce9d591fe
Claudia <c.agosti006@studenti.unibs.it>
2022-06-01 10:01:02 +0200

    Changes by greta

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,20 @@ import java.io.IOException;
 
 public class Main {
 
-    //creazione variabili xij del modello
+    //creazione variabili xij
     private static GRBVar[][] aggiungiVariabilix(GRBModel model, int nVertici) throws GRBException {
         GRBVar[][] xij = new GRBVar[nVertici][nVertici];
 
-        for (int i = 0; i < vertiveV; i++) {
+        for (int i = 0; i < nVertici; i++) {
 
             for (int j = 0; j < nVertici; j++) {
-                xij[i][j] = model.addVar(0, 1, 0, GRB.INTEGER, "x" + i + j);
+                xij[i][j] = model.addVar(0, 1, 0, GRB.BINARY, "x" + i + j);
             }
         }
         return xij;
     }
 
-    //creazione variabili uij del modello
+    //creazione variabili u
     private static GRBVar[] aggiungiVariabiliu(GRBModel model, int nVertici) throws GRBException {
         GRBVar[] u = new GRBVar[nVertici];
 
@@ -29,18 +29,24 @@ public class Main {
 
         return u;
     }
-    //aggiungi variabili z
+
+    /**
+     * variabili z utilizzate vincolo [d] terza richiesta
+     */
     private static GRBVar[] aggiungiVariabiliz(GRBModel model, int nVertici) throws GRBException {
         GRBVar[] z = new GRBVar[nVertici];
 
         for (int j = 0; j < 4; j++) {
-            z[j] = model.addVar(0, 1, 0, GRB.BINARY, "x" +  j);
+            z[j] = model.addVar(0, 1, 0, GRB.BINARY, "z" +  j);
         }
 
         return z;
     }
 
-    //aggiunta funzione obiettivo al modello
+    /**
+     * aggiunta funzione obiettivo modello principale
+     */
+
     private static void aggiungiFunzioneObiettivo(GRBModel model, int [][] cij, GRBVar [][] xij, int nVertici) throws GRBException {
         GRBLinExpr funzione_obiettivo = new GRBLinExpr();
 
@@ -54,10 +60,28 @@ public class Main {
         model.set(GRB.IntAttr.ModelSense, GRB.MINIMIZE);
     }
 
+    /**
+     * aggiunta funzione obiettivo al modello terza richiesta
+     */
+    private static void aggiungiFunzioneObiettivo(GRBModel model, int [][] cij, GRBVar [][] xij, int nVertici, GRBVar y, int l) throws GRBException {
+        GRBLinExpr funzione_obiettivo = new GRBLinExpr();
+        //sommatoria Cij * Xij
+        for (int i = 0; i < nVertici; i++){
+            for (int j = 0; j < nVertici; j++){
+                if(i!=j)
+                    funzione_obiettivo.addTerm(cij[i][j], xij[i][j]);
+            }
+        }
+        funzione_obiettivo.addTerm(l, y);
+        model.setObjective(funzione_obiettivo);
+        model.set(GRB.IntAttr.ModelSense, GRB.MINIMIZE);
+    }
 
 
+    /**
+     * vincolo 1: sommatoria Xij = 1    j fissato
+     */
     private static void aggiungiVincolo1(GRBModel model, GRBVar[][] xij,int nVertici) throws GRBException {
-
         for(int j=0; j<nVertici; j++){
             GRBLinExpr expr = new GRBLinExpr();
             for(int i=0; i<nVertici; i++){
@@ -70,8 +94,10 @@ public class Main {
 
     }
 
+    /**
+     * vincolo 2: sommatoria Xij = 1    i fissato
+     */
     private static void aggiungiVincolo2(GRBModel model, GRBVar[][] xij,int nVertici) throws GRBException {
-
         for (int i = 0; i < nVertici; i++){
             GRBLinExpr expr = new GRBLinExpr();
             for (int j = 0; j < nVertici; j++){
@@ -84,7 +110,9 @@ public class Main {
     }
 
     private static void aggiungiVincolo3(GRBModel model, GRBVar[][] xij,int nVertici, GRBVar [] u) throws GRBException {
-        int cost = (nVertici - 1);
+        //Ui - Uj + (n-1)Xij <= n - 2
+
+        int cost = (nVertici - 2);
         GRBLinExpr expr5 = new GRBLinExpr();
         expr5.addConstant(cost);
         for (int i = 1; i < nVertici; i++){
@@ -93,17 +121,17 @@ public class Main {
                 int indicej = j-1;
                 if(i!=j) {
                     GRBLinExpr expr = new GRBLinExpr();
-                    expr.addTerm(nVertici, xij[i][j]);
+                    expr.addTerm((nVertici -1), xij[i][j]);
                     expr.addTerm(1, u[indicei]);
                     expr.addTerm(-1, u[indicej]);
-                    //System.out.println(i+","+j+" "+expr.getCoeff(i));
                     model.addConstr(expr, GRB.LESS_EQUAL,expr5, "vincolo di con ui e uj" + j);
                 }
             }
         }
     }
 
-    private static void aggiungiVincolo4(GRBModel model,GRBVar[][] xij, int [][] cij, int nVertici,double val_fobj  ){
+    private static void aggiungiVincolo4(GRBModel model,GRBVar[][] xij, int [][] cij, int nVertici,double val_fobj  )throws GRBException{
+        //valore funzione obiettivo = min calcolato modello precedente
         GRBLinExpr funzione_obiettivo_vincolo = new GRBLinExpr();
         GRBLinExpr expr5 = new GRBLinExpr();
         expr5.addConstant(val_fobj);
@@ -115,11 +143,35 @@ public class Main {
                 }
             }
         }
-        try {
-            model.addConstr(funzione_obiettivo_vincolo, GRB.EQUAL, expr5, "vincolo funzione obiettivo" );
-        } catch (GRBException e) {
-            e.printStackTrace();
+        model.addConstr(funzione_obiettivo_vincolo, GRB.EQUAL, expr5, "vincolo funzione obiettivo" );
+    }
+
+    private static void aggiungiVincoloA(GRBModel model, GRBVar[][] xij, int [][] cij, int nVertici, int a, int v, int l, GRBVar y)throws GRBException{
+
+        GRBLinExpr expr = new GRBLinExpr();
+        GRBLinExpr funzione_obiettivo = new GRBLinExpr();
+
+        for (int i=0; i<nVertici; i++){
+            expr.addTerm(cij[v][i], xij[v][i]);
+            expr.addTerm(cij[i][v], xij[i][v]);
         }
+
+        for (int i = 0; i < nVertici; i++){
+            for (int j = 0; j < nVertici; j++){
+                if(i!=j)
+                    funzione_obiettivo.addTerm((double)a*cij[i][j]/100, xij[i][j]);
+            }
+        }
+        funzione_obiettivo.addTerm((double)a*l/100, y);
+
+        model.addConstr(expr, GRB.LESS_EQUAL, funzione_obiettivo, "vincoloA_vj" );
+/*
+        GRBLinExpr expr1 = new GRBLinExpr();
+        for (int i=0; i<nVertici; i++){
+            expr1.addTerm(cij[i][v], xij[i][v]);
+        }
+        model.addConstr(expr1, GRB.LESS_EQUAL, funzione_obiettivo, "vincoloA" );*/
+
     }
 
     private static void aggiungiVincoloB(GRBModel model, GRBVar[][] xij, int [][] cij,int c, int nVertici, int [] latob, int l, GRBVar y, double m)throws GRBException{
@@ -168,7 +220,6 @@ public class Main {
         for (int i = 0; i < 4; i++){
             expr5.addTerm(i, z[i]);
         }
-
         //Xg+Xh+Xi = 0 Z0+  Z1 + 2 Z2 + 3 Z3
         model.addConstr(expr, GRB.EQUAL,expr5, "vincoloD" );
 
@@ -188,17 +239,17 @@ public class Main {
         int verticeV = 2;
         int a=7; //vincolo1: il costo dei lati incidenti al vertice v sia al massimo il a% del costo totale del ciclo
         int [] latob ={6, 25};
-        int latoc=114;
+        int c=114;
         int[] latod = {36, 1 };
         int[] latoe = {13, 10};
         int[] latof = {32, 2};
         int[] latog = {2, 8};
         int[] latoh = {16, 39};
         int[] latoi = {7, 9};
-        int latol = 2;
+        int l = 2;
 
         String nome_file = "coppia25.txt";
-
+//----------------------LETTURA DATI DA FILE-------------------
         try {
             // apre in lettura
             BufferedReader filebuf = new BufferedReader(new FileReader(nome_file));
@@ -224,70 +275,32 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e);
         }
-
+//------------------------------------------------------------------------------
         GRBEnv env;
         try {
             env = new GRBEnv("elaborato1Gurobi.log");
-            //env.set(GRB.IntParam.Presolve, 0);
-            //env.set(GRB.IntParam.Method, 0);
 
+//*************************** QUESITO 1 ********************************************************************************
             GRBModel model = new GRBModel(env);
 
-//-------------- AGGIUNTA VARIABILI -----------------------------------
+    //-------------- AGGIUNTA VARIABILI -----------------------------------
             GRBVar[][] xij = aggiungiVariabilix(model, n_vertici); //variabile binaria  ed è uguale a 1 se l'arco (i,j) appartiene al circuito, altrimenti xij=0
             GRBVar[] u = aggiungiVariabiliu(model, n_vertici);
-// -------------- FUNZIONE OBIETTIVO ----------------------------------
+
+    // -------------- FUNZIONE OBIETTIVO ----------------------------------
             aggiungiFunzioneObiettivo(model, cij, xij, n_vertici);
 
-
-//-------------------- VINCOLI ----------------------------------------
+    //-------------------- VINCOLI ----------------------------------------
             aggiungiVincolo1(model, xij, n_vertici);
             aggiungiVincolo2(model, xij, n_vertici);
             aggiungiVincolo3(model, xij, n_vertici, u);
 
             model.optimize();   //ottimizzazione
-//----------------------------------------------------------------------
+    //----------------------------------------------------------------------
 
+//*************************** QUESITO 2 ********************************************************************************
+            double objval= model.get(GRB.DoubleAttr.ObjVal);
 
-            System.out.println("\nGRUPPO gruppo_25");
-            System.out.println("Componenti: Brognoli e Agosti\n\n");
-//------------------------------- QUESITO 1 ---------------------------------------------------------------------------
-            System.out.println("QUESITO I:");
-            System.out.println("funzione obiettivo = " + model.get(GRB.DoubleAttr.ObjVal));
-            System.out.print( "ciclo ottimo 1 = [");
-
-            /*for(int i=0; i<n_vertici;i++) {
-                for (int j = 0; j < n_vertici; j++) {
-                    System.out.printf("%.1f \t",xij[i][j].get(GRB.DoubleAttr.X));
-                }
-                System.out.println();
-            }*/
-            int i=0;
-            int j;
-
-            do{
-                for(j=0; j<n_vertici; j++){
-                    if(xij[i][j].get(GRB.DoubleAttr.X)!=0){
-                        System.out.print(i+", ");
-                        break;
-                    }
-                }
-                i=j;
-            }while(j!=0);
-            System.out.print("0]");
-            System.out.println();
-             // visualizzazione matrice
-            // System.out.print("\n"+i);
-
-           /* for(int k=0; k<n_vertici;k++) {
-                for (int l = 0; l < n_vertici; l++) {
-                    System.out.print((xij[k][l].get(GRB.DoubleAttr.X))+"\t");
-                }
-                System.out.println();
-            }
-            */
-
-//---------------------QUESITO II--------------------------------------------------------------------------------------
             GRBModel model_II = new GRBModel(env);
 
             GRBVar[][] xij_II = aggiungiVariabilix(model_II, n_vertici); //variabile binaria  ed è uguale a 1 se l'arco (i,j) appartiene al circuito, altrimenti xij=0
@@ -304,16 +317,71 @@ public class Main {
 
             model_II.optimize();    //ottimizzazione
 
-            System.out.print( "ciclo ottimo 2 = [");
+//*************************** QUESITO 3 ********************************************************************************
+            GRBModel model_III = new GRBModel(env);
 
-          /*   for(int k=0; k<n_vertici;k++) {
-                for (int l = 0; l < n_vertici; l++) {
-                    System.out.print((xij[k][l].get(GRB.DoubleAttr.X))+"\t");
+            GRBVar[][] xij_III = aggiungiVariabilix(model_III, n_vertici); //variabile binaria  ed è uguale a 1 se l'arco (i,j) appartiene al circuito, altrimenti xij=0
+            GRBVar[] u_III = aggiungiVariabiliu(model_III, n_vertici);
+            GRBVar[] z_III =aggiungiVariabiliz(model_III,n_vertici);
+            aggiungiFunzioneObiettivo(model_III, cij, xij_III, n_vertici, z_III[3], l);
+
+            aggiungiVincolo1(model_III, xij_III, n_vertici);
+            aggiungiVincolo2(model_III, xij_III, n_vertici);
+            aggiungiVincolo3(model_III, xij_III, n_vertici, u_III);
+            aggiungiVincoloA(model_III, xij_III, cij, n_vertici, a, verticeV, l, z_III[3]);
+
+            //---------- calcolo M --------
+            /*
+             * vincolo:  f <= cy + (1-y) M
+             * come valore di M posso prendere:
+             *  - max f ....risolvere il modello
+             *  - valore molto grande deciso arbitrariamente
+             *  - sommo il costo massimo di ogni riga della matrice dei costi (costi sulla diagonale = 0)
+             */
+            double M=0;
+            double max;
+            for(int i=0; i<n_vertici; i++){
+                max=0;
+                for(int j=0; j<n_vertici; j++){
+                    if(cij[i][j]>max){
+                        max=cij[i][j];
+                    }
                 }
-                System.out.println();
+                M+=max;
             }
-            */
+            aggiungiVincoloB(model_III, xij_III, cij,c,  n_vertici,latob,l,  z_III[3], M);
+            aggiungiVincoloC(model_III,xij_III, latoe,latof, latod);
+            aggiungiVincoloD(model_III, z_III, xij_III, latog,  latoh, latoi);
 
+            model_III.optimize();   //ottimizzazion
+
+
+
+//************************** OUTPUT ******************************************************************************
+            int i=0;
+            int j;
+            System.out.println("\nGRUPPO gruppo_25");
+            System.out.println("Componenti: Brognoli e Agosti\n\n");
+
+//------------------------------- QUESITO 1 ---------------------------------------------------------------------------
+            System.out.println("QUESITO I:");
+            System.out.println("funzione obiettivo = " + model.get(GRB.DoubleAttr.ObjVal));
+            System.out.print( "ciclo ottimo 1 = [");
+            do{
+                for(j=0; j<n_vertici; j++){
+                    if(xij[i][j].get(GRB.DoubleAttr.X)!=0){
+                        System.out.print(i+", ");
+                        break;
+                    }
+                }
+                i=j;
+            }while(j!=0);
+            System.out.print("0]");
+            System.out.println("\n");
+
+//---------------------QUESITO II--------------------------------------------------------------------------------------
+            System.out.println("QUESITO II:");
+            System.out.print( "ciclo ottimo 2 = [");
             i=0;
             do{
                 for(j=0; j<n_vertici; j++){
@@ -326,42 +394,11 @@ public class Main {
             }while(j!=0);
 
             System.out.print("0]");
-            System.out.println();
-
-
+            System.out.println("\n");
 
 
 //---------------------QUESITO III--------------------------------------------------------------------------------------
-            GRBModel model_III = new GRBModel(env);
-
-            GRBVar[][] xij_III = aggiungiVariabilix(model_III, n_vertici); //variabile binaria  ed è uguale a 1 se l'arco (i,j) appartiene al circuito, altrimenti xij=0
-            GRBVar[] u_III = aggiungiVariabiliu(model_III, n_vertici);
-            GRBVar[] z_III =aggiungiVariabiliz(model_III,n_vertici);
-           // GRBVar y= model.addVar( 0, 1, 0, GRB.BINARY, "Y");
-            aggiungiFunzioneObiettivo(model_III, cij, xij_III, n_vertici, z_III[3], l);
-
-            aggiungiVincolo1(model_III, xij_III, n_vertici);
-            aggiungiVincolo2(model_III, xij_III, n_vertici);
-            aggiungiVincolo3(model_III, xij_III, n_vertici, u_III);
-            aggiungiVincoloA(model_III, xij_III, cij, n_vertici, a, verticeV, l, y);
-
-            //----------calcolo costo massimo di un percorso------
-            double M=0;
-            double max;
-            for(i=0; i<n_vertici; i++){
-                max=0;
-                for(j=0; j<n_vertici; j++){
-                    if(cij[i][j]>max){
-                        max=cij[i][j];
-                    }
-                }
-                M+=max;
-            }
-            aggiungiVincoloB(model_III, xij_III, cij,c,  n_vertici,latob,l,  y, M);
-            aggiungiVincoloC(model_III,xij_III, cij, latoe,latof, latod);
-            aggiungiVincoloD(model_III, z_III, xij_III, latog,  latoh, latoi);
-
-            model_III.optimize();   //ottimizzazion
+            System.out.println("QUESITO III:");
             System.out.println("funzione obiettivo = " + model_III.get(GRB.DoubleAttr.ObjVal));
             System.out.print( "ciclo ottimo 3 = [");
 
